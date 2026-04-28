72ac16bc14c21b925c5c7742c7704342f9cdee65
Meehw <ravenblack9798@gmail.com>
2026-04-29 06:47:52 +0800

    Student Management System

--- index.php
+++ index.php
@@ -104,9 +104,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </button>
             </form>
 
-            <div class="text-center mt-4">
-                <small class="text-muted">Default: admin / admin123</small>
-            </div>
+
         </div>
     </div>
 </div>
